ad3061ac9d83493da19b40a317393042d5cdfd3d
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-10-11 01:25:50 +0900

    add

---
+++ chap2/js/typeof.js
@@ -0,0 +1,11 @@
+var num = 1;
+console.log(typeof num); // 結果:number
+
+var str = 'こんにちは';
+console.log(typeof str); // 結果:string
+
+var flag = true;
+console.log(typeof flag); // 結果:boolean
+
+var obj = {x:1, y:2};
+console.log(typeof obj); // 結果:object
