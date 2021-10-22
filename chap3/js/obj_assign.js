c3ef879d3bc1699df2f6e9a6550897e1f2e21074
Takuya Kogawa <tk.ngskgtc.vt@gmail.com>
2021-10-22 22:12:44 +0900

    add

---
+++ chap3/js/obj_assign.js
@@ -0,0 +1,39 @@
+// assignメソッドの注意点3点
+// 1.同名のプロパティは、上書き処理される（この例では、name）
+// 2.再帰的なマージには非対応（この例では、descriptionプロパティはまるごと上書き）
+// 3.破壊的なメソッドである
+
+let pet = {
+	type: 'スノーホワイトハムスター',
+	name: 'キラ',
+	description: {
+		birth: 'C.E.55-05-18'
+	}
+};
+
+let pet2 = {
+	name: 'キラ・ヤマト',
+	color: 'brown',
+	description: {
+		food: '甘い物（おはぎなど）'
+	}
+};
+
+let pet3 = {
+	weight: '58',
+	photo: 'http://www.wings.msn.to/img/ham.jpg'
+};
+
+// 3の回避策（からのオブジェクトにマージさせる）
+let merged = Object.assign({}, pet, pet2, pet3);
+console.log(merged);
+console.log(pet);
+
+Object.assign(pet, pet2, pet3);
+console.log(pet);
+// color: "brown"
+// description: {food: '甘い物（おはぎなど）'}
+// name: "キラ・ヤマト"
+// photo: "http://www.wings.msn.to/img/ham.jpg"
+// type: "スノーホワイトハムスター"
+// weight: "58"
